93566248929b4b225709056a2516ee47af59dce3
Denis Danilov <d.danilov@duotek.ru>
2025-04-04 19:02:10 +0300

    Temp

--- components/Menu.vue
+++ components/Menu.vue
@@ -91,47 +91,50 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="menu" ref="menuTemplate">
-    <div class="menu__company-name" ref="menuCompanyNameTemplate">TOP1ANIMATOR</div>
-    <ul class="menu__items" ref="menuItems1Template" v-gsap.fromInvisible.from='{ delay: 0.1, y: -10 }'>
-      <li v-for="item in items">
-        <a :href="item.link" ref="menuItemsTemplate">{{ item.label }}</a>
-      </li>
-    </ul>
-    <div class="burger-container" @click="openMenu">
-      <svg height="50" width="50">
-        <use xlink:href="/sprite.svg#burger" />
-      </svg>
-    </div>
-  </div>
-  <div class="menu-mobile" :class="{'active': mobIsActive}" ref="menuMobileTemplate">
-    <div>
-      <div class="menu-mobile__header-container">
-        <div class="menu-mobile__company-name" ref="menuCompanyNameTemplate">TOP1ANIMATOR</div>
-        <svg height="28" width="28" @click="closeMenu">
-          <use xlink:href="/sprite.svg#close" />
+  <div>
+    <div class="menu__company-name --mobile">TOP1ANIMATOR</div>
+    <div class="menu" ref="menuTemplate">
+      <div class="menu__company-name" ref="menuCompanyNameTemplate">TOP1ANIMATOR</div>
+      <ul class="menu__items" ref="menuItems1Template" v-gsap.fromInvisible.from='{ delay: 0.1, y: -10 }'>
+        <li v-for="item in items">
+          <a :href="item.link" ref="menuItemsTemplate">{{ item.label }}</a>
+        </li>
+      </ul>
+      <div class="burger-container" @click="openMenu">
+        <svg height="50" width="50">
+          <use xlink:href="/sprite.svg#burger" />
         </svg>
       </div>
-      <ul class="menu-mobile__items">
-        <li v-for="item in items" ><a :href="item.link">{{ item.label }}</a></li>
-      </ul>
     </div>
-    <div>
-      <div class="menu-mobile__phone-container">
-        <div class="menu-mobile__messengers">
-          <div class="menu-mobile__messenger" @click="onClickWhatsapp">
-            <div class="menu-mobile__messenger-label">
-              <a class="menu-mobile__whatsapp-link">Написать в <span style="color: #40c351;">Whatsapp</span></a>
-            </div>
-          </div>
-          <div class="menu-mobile__messenger" @click="onClickTelegram">
-            <div class="menu-mobile__messenger-label">
-              <a class="menu-mobile__telegram-link">&nbsp;Написать в <span style="color: #29b6f6;">Telegram</span>&nbsp;</a>
-            </div>
-          </div>
+    <div class="menu-mobile" :class="{'active': mobIsActive}" ref="menuMobileTemplate">
+      <div>
+        <div class="menu-mobile__header-container">
+          <div class="menu-mobile__company-name" ref="menuCompanyNameTemplate">TOP1ANIMATOR</div>
+          <svg height="28" width="28" @click="closeMenu">
+            <use xlink:href="/sprite.svg#close" />
+          </svg>
         </div>
-        <div>Бесплатная консультация</div>
-        <a href="tel:+79999999999">+7 (999) 999-99-99</a>
+        <ul class="menu-mobile__items">
+          <li v-for="item in items" ><a :href="item.link">{{ item.label }}</a></li>
+        </ul>
+      </div>
+      <div>
+        <div class="menu-mobile__phone-container">
+          <div class="menu-mobile__messengers">
+            <div class="menu-mobile__messenger" @click="onClickWhatsapp">
+              <div class="menu-mobile__messenger-label">
+                <a class="menu-mobile__whatsapp-link">Написать в <span style="color: #40c351;">Whatsapp</span></a>
+              </div>
+            </div>
+            <div class="menu-mobile__messenger" @click="onClickTelegram">
+              <div class="menu-mobile__messenger-label">
+                <a class="menu-mobile__telegram-link">&nbsp;Написать в <span style="color: #29b6f6;">Telegram</span>&nbsp;</a>
+              </div>
+            </div>
+          </div>
+          <div>Бесплатная консультация</div>
+          <a href="tel:+79999999999">+7 (999) 999-99-99</a>
+        </div>
       </div>
     </div>
   </div>
